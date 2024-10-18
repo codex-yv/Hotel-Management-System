3002b17dfc03255eebe7f229f576dcdf024c44f8
codex-yv <yourajverma960@gmail.com>
2024-10-18 14:54:32 +0530

    modified

--- zconnect.py
+++ zconnect.py
@@ -1,9 +1,13 @@
 from mysql.connector import*
+file22=open("2sql.txt",'r')
+sqlp=file22.read()
+# print(sqlp)
+file22.close()
 try:
     con=connect(
         host='localhost',
         user='root',
-        password='ssap856'
+        password=sqlp
     )
     print("Connection Established!")
 except Error as e:
